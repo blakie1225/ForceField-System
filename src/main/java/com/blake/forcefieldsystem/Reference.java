d16f8c3b1e5bd24a39abe50f954e84dfa219db29
Blake <blakie1225@gmail.com>
2016-08-19 21:49:46 +1200

    First Commit!

--- src/main/java/com/blake/forcefieldsystem/Reference.java
+++ src/main/java/com/blake/forcefieldsystem/Reference.java
@@ -2,9 +2,9 @@ package com.blake.forcefieldsystem;
 
 public class Reference {
 
-    public static final String MODID = "forcefieldsystem";
+    public static final String MODID = "ForceFieldSystem";
     public static final String MODNAME = "ForceField System";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.7.10-1.0";
 
     public static final String CLIENT_PROXY = "com.blake.forcefieldsystem.proxy.ClientProxy";
     public static final String SERVER_PROXY = "com.blake.forcefieldsystem.proxy.ServerProxy";
